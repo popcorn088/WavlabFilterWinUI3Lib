0bfb534751bcb48c2e50157ee36de11386e3239f
popcorn <popcorn088@gmail.com>
2024-05-06 16:59:33 +0900

    ZeroOrderHoldConsoleアップ

---
+++ ZeroOrderHoldConsole/ZeroOrderHoldConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using WavlabFilterWinUI3Lib;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WavlabFilterClient client = new WavlabFilterClient();
+            client.SendCoefs(new double[] { 0.2, 0.2, 0.2, 0.2, 0.2 }, new double[0]);
+        }
+    }
+}
